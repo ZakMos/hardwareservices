385e4e38fad8780c74f7afe381501740c24aafc7
zakmos <z.zza@hotmail.com>
2022-03-23 17:57:13 +0100

    added mobile json data

---
+++ src/jsonData/productsIphone.js
@@ -0,0 +1,78 @@
+export const productsIphone =   
+[
+  {   
+    "name": "Apple", 
+    "label": "Iphone 13 Pro",
+    "id": "01",
+    "mainPic": require('../images/iphone/iphone-13-pro.jpg'), 
+    "mainPicName": "iphone-13-pro",
+    "value": "IPhone 13", 
+    "displayPic": require('../images/display.png'),
+    "items": true,
+    "originalDisplay": "ORIGINAL DEF. DISPLAY",
+    "originalDisplayText": "Display Repair (Orig.Ref.)",
+    "displayPrice": " 379,90€",
+    "copyDisplay": "COPY DISPLAY",
+    "copyDisplayText": "Copy Display (OLED Display)",
+    "copyDisplayPrice": "319,90€",
+    "backGlassPic": require('../images/display.png'),
+    "backGlass": "BACK GLASS REPAIR",
+    "backGlassText": "With Laser Technique",
+    "backGlassPrice": "249,90€",
+    "chargePortPic": require('../images/display.png'),
+    "chargePort": "CHARGING PORT",
+    "chargePortText": "Replace Charging Port",
+    "chargePortPrice": "199,90€",
+    "batteryPic": require('../images/display.png'),
+    "battery": "Battery",
+    "batteryText": "Replacing the Battery",
+    "batteryPrice": "149,90€",
+    "soundPic": require('../images/display.png'),
+    "sound": "SOUND",
+    "soundText": "Repair",
+    "soundPrice": "149,90€",
+  },
+  {   
+    "name": "Apple", 
+    "label": "Iphone 12",
+    "id": "02",
+    "mainPic": require('../images/iphone/iphone-12.jpg'), 
+    "mainPicName": "iphone-12",
+    "value": "IPhone 12", 
+    "displayPic": require('../images/display.png'),
+    "items": true,
+    "originalDisplay": "ORIGINAL DEF. DISPLAY",
+    "originalDisplayText": "Display Repair (Orig.Ref.)",
+    "displayPrice": " 379,90€",
+    "copyDisplay": "COPY DISPLAY",
+    "copyDisplayText": "Copy Display (OLED Display)",
+    "copyDisplayPrice": "319,90€",
+    "backGlassPic": require('../images/display.png'),
+    "backGlass": "BACK GLASS REPAIR",
+    "backGlassText": "With Laser Technique",
+    "backGlassPrice": "249,90€",
+    "chargePortPic": require('../images/display.png'),
+    "chargePort": "CHARGING PORT",
+    "chargePortText": "Replace Charging Port",
+    "chargePortPrice": "199,90€",
+    "batteryPic": require('../images/display.png'),
+
+
+
+
+
+
+
+
+
+
+
+    "batteryText": "Replacing the Battery",
+    "batteryPrice": "149,90€",
+    "soundPic": require('../images/display.png'),
+    "sound": "SOUND",
+    "soundText": "Repair",
+    "soundPrice": "149,90€",
+  },
+]
+    
---
+++ src/jsonData/productsSamsung.js
@@ -0,0 +1,30 @@
+export const productsSamsung =   
+[
+  { 
+    "name": "Samsung",
+    "label": "Samsung Galaxy S22 Ultra",
+    "id": "01", 
+    "mainPic": require('../images/samsung/galaxy-s22-ultra.jpg'), 
+    "value": "Galaxy S22 Ultra", 
+    "description": "Galaxy S22 Ultra",
+    "price": "€ 1500"
+  },
+  { 
+    "name": "Samsung", 
+    "label": "Samsung Note 10",
+    "id": "02",
+    "mainPic": require('../images/samsung/Samsung-note10.jpg'), 
+    "description": "Note 10",
+    "price": "€ 400"
+  },
+  { 
+    "name": "Samsung",
+    "label": "Samsung Galaxy 11",
+    "id": "03", 
+    "value": "Galaxy 11", 
+    "description": "Galaxy 11",
+    "price": "€ 10"
+  }
+  
+]
+    
